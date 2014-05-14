a028738d1bd13ea4a9342b1675e17ba05c8aaa3d
Felipe Zavan <emailbloggers@gmail.com>
2014-05-14 18:44:19 -0300

    Version require must come first

--- lib/google_maps_api/core.rb
+++ lib/google_maps_api/core.rb
@@ -1,8 +1,8 @@
+require "google_maps_api/core/version"
 require "google_maps_api/core/metric"
 require "google_maps_api/core/duration"
 require "google_maps_api/core/distance"
 require "google_maps_api/core/coordinate"
-require "google_maps_api/core/version"
 
 module GoogleMapsAPI
   module Core; end
